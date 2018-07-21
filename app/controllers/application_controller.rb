d8f813da41e053b6521e067d6be72f0c71bd03a5
Kayla Reopelle <kreopelle@gmail.com>
2018-07-21 03:23:13 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require_relative 'config/environment'
+
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
@@ -6,4 +8,28 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+end
+
+
+
+
+class App < Sinatra::Base
+  configure do
+    enable :Sessions
+    set :session_secret, "pickles"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @item = params[:item]
+
+    session[:item] = @item
+    @session = session
+
+    erb :checkout
+  end
+
 end
